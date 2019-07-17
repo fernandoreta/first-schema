7d1350c2843ea8f8974a2e249835f509cf31fda5
Fernando Wertt <fer19_reta@hotmail.com>
2019-07-16 19:10:29 -0500

    create the first index.html with options

--- schematics/src/schematics/index.ts
+++ schematics/src/schematics/index.ts
@@ -3,8 +3,15 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schematics(_options: any): Rule {
+interface MyOptions {
+  name: string;
+  lastName: string;
+}
+export function schematics(_options: MyOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    return tree.create("index.html", `
+    <p>This is just an example to schematics>/p>
+    <p>My name is ${_options.name} ${_options.lastName}</p>
+    `);
   };
 }
