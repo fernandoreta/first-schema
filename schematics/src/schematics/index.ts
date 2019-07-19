b40170bf4d3b4b885ad3652ed65ae54806b0ac1c
Fernando Wertt <fer19_reta@hotmail.com>
2019-07-19 11:21:17 -0500

    creating a normal html file wt interface

--- schematics/src/schematics/index.ts
+++ schematics/src/schematics/index.ts
@@ -3,8 +3,14 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function schematics(_options: any): Rule {
+interface MyOptions {
+  name: string;
+}
+
+export function schematics(_options: MyOptions): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    return tree.create("index.html", `
+    <p>My name is ${_options.name}</p>
+    `);
   };
 }
