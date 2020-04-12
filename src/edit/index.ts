3c714447462916d2c32548fb4d33ee0f73f4751c
Fernando Wertt <fer19_reta@hotmail.com>
2020-04-11 22:11:27 -0500

    addtoHTML

--- src/edit/index.ts
+++ src/edit/index.ts
@@ -3,8 +3,20 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function edit(_options: any): Rule {
+interface propMenu {
+  prop: string
+}
+export function edit(_options: propMenu): Rule {
   return (tree: Tree, _context: SchematicContext) => {
+    const content: Buffer | null = tree.read("./src/app/components/default-name.component.html");
+    let strContent: string = '';
+    if(content) strContent = content.toString();
+
+    const appendIndex = strContent.indexOf('</ul>');
+    const content2Append =  `<li><a href="/${_options.prop}">${_options.prop}</a></li> \n`;
+    const updatedContent = strContent.slice(0, appendIndex) + content2Append + strContent.slice(appendIndex);
+
+    tree.overwrite("./src/app/components/default-name.component.html", updatedContent);
     return tree;
   };
 }
